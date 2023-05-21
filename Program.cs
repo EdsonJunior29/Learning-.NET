0fceb6f28ce20ea5c0c8a0dbc1cd717a71c2587f
Edson Junior <edson.junior@mereo.com>
2023-05-21 09:44:10 -0300

    Passing parameters via body
    Request type Post.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello Edson!");
 
+app.MapPost("/user", (User user) =>
+{
+    return user.Code + " - " + user.Name;
+});
 app.Run();
+
+
+public class User {
+    public int Code { get; set; }
+    public string Name { get; set; }
+}
