2562f5d960654127a04bd10ebd5ca526af614f9b
Edson Junior <edson.junior@mereo.com>
2023-05-21 10:12:59 -0300

    Sending data via URL
    Sending data via URL
    Using queryParams and Routes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -7,8 +9,21 @@ app.MapPost("/user", (User user) =>
 {
     return user.Code + " - " + user.Name;
 });
-app.Run();
 
+app.MapGet("/getuser", ([FromQuery] string code , [FromQuery] string name) =>
+{
+    // Requisição via postman (http://localhost:5124/getuser?code=1&name=Edson)
+    return code + " - " + name;
+});
+
+
+app.MapGet("/getuser2/{code}/{name}", ([FromRoute] string code, [FromRoute] string name) =>
+{
+    // Requisição via postman (http://localhost:5124/getuser2/1/EdsonJunior)
+    return code + " - " + name;
+});
+
+app.Run();
 
 public class User {
     public int Code { get; set; }
