0859200e32ee65f33cbb383c923a3cfa5af99bb8
Edson Junior <edson.junior@mereo.com>
2023-05-22 17:22:20 -0300

    Testing on endpoints

--- Program.cs
+++ Program.cs
@@ -8,39 +8,23 @@ app.MapGet("/", () => "Hello Edson!");
 
 app.MapPost("/user", (User user) =>
 {
-    return user.Code + " - " + user.Name;
+    UserRepository.Add(user);
 });
 
-app.MapGet("/getuser", ([FromQuery] string code , [FromQuery] string name) =>
+app.MapGet("/user/{code}", ([FromRoute] int code) =>
 {
-    // Requisi��o via postman (http://localhost:5124/getuser?code=1&name=Edson)
-    return code + " - " + name;
-});
-
-
-app.MapGet("/getuser2/{code}/{name}", ([FromRoute] string code, [FromRoute] string name) =>
-{
-    // Requisi��o via postman (http://localhost:5124/getuser2/1/EdsonJunior)
-    return code + " - " + name;
-});
-
-app.MapGet("/getuser3", (HttpRequest request) =>
-{
-    //http://localhost:5124/getuser3 
-    //Header = product-code(Key) = 20(value)
-    //OBS:  O Header da aplica��o � um Dictionary
-    return request.Headers["product-code"].ToString();
+    return UserRepository.GetByCode(code);
 });
 
 app.Run();
 
-public static class User {
+public class User {
     public int Code { get; set; }
     public string Name { get; set; }
 }
 
 public static class UserRepository {
-    public List<User> Users {get; set;}
+    public static List<User> Users {get; set;}
 
     public static void Add(User user) {
         if (Users == null) {
